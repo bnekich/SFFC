1274e996b344fb99b3c6a984a45ed161e1c2c613
bnekich <bnekich@gmail.com>
2025-05-15 16:31:16 -0500

    deployment changes

--- case-management/resources/views/home.blade.php
+++ case-management/resources/views/home.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home - SFFC Case Management</title>
     <link rel="stylesheet" href="//fonts.bunny.net/css?family=Nunito">
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    @vite(['resources/js/app.ts', 'resources/sass/app.scss'])
 </head>
 
 <body>
