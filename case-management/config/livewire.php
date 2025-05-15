b162046f3b9286254d49b8316b2e1487ba0a0ade
bnekich <bnekich@gmail.com>
2025-05-15 12:39:15 -0500

    prepare for distro

--- case-management/config/livewire.php
+++ case-management/config/livewire.php
@@ -63,27 +63,18 @@ return [
     |
     */
 
-    // 'temporary_file_upload' => [
-    //     'disk' => null,        // Example: 'local', 's3'              | Default: 'default'
-    //     'rules' => null,       // Example: ['file', 'mimes:png,jpg']  | Default: ['required', 'file', 'max:12288'] (12MB)
-    //     'directory' => null,   // Example: 'tmp'                      | Default: 'livewire-tmp'
-    //     'middleware' => null,  // Example: 'throttle:5,1'             | Default: 'throttle:60,1'
-    //     'preview_mimes' => [   // Supported file types for temporary pre-signed file URLs...
-    //         'png', 'gif', 'bmp', 'svg', 'wav', 'mp4',
-    //         'mov', 'avi', 'wmv', 'mp3', 'm4a',
-    //         'jpg', 'jpeg', 'mpga', 'webp', 'wma',
-    //     ],
-    //     'max_upload_time' => 5, // Max duration (in minutes) before an upload is invalidated...
-    //     'cleanup' => true, // Should cleanup temporary uploads older than 24 hrs...
-    // ],
     'temporary_file_upload' => [
-        'disk' => 'public', // Use 's3' for production
-        'rules' => ['file', 'max:10240', 'mimes:pdf,doc,docx,xlsx,jpg,png,txt,csv,odt'],
-        'directory' => 'livewire-tmp',
-        'middleware' => 'throttle:60,1',
-        'preview_mimes' => ['png', 'jpg'],
-        'max_upload_time' => 5,
-        'cleanup' => true,
+        'disk' => null,        // Example: 'local', 's3'              | Default: 'default'
+        'rules' => null,       // Example: ['file', 'mimes:png,jpg']  | Default: ['required', 'file', 'max:12288'] (12MB)
+        'directory' => null,   // Example: 'tmp'                      | Default: 'livewire-tmp'
+        'middleware' => null,  // Example: 'throttle:5,1'             | Default: 'throttle:60,1'
+        'preview_mimes' => [   // Supported file types for temporary pre-signed file URLs...
+            'png', 'gif', 'bmp', 'svg', 'wav', 'mp4',
+            'mov', 'avi', 'wmv', 'mp3', 'm4a',
+            'jpg', 'jpeg', 'mpga', 'webp', 'wma',
+        ],
+        'max_upload_time' => 5, // Max duration (in minutes) before an upload is invalidated...
+        'cleanup' => true, // Should cleanup temporary uploads older than 24 hrs...
     ],
 
     /*
