b705a3cec762ae652ae4a237f52a675658f08aa6
bnekich <bnekich@gmail.com>
2025-10-22 17:02:14 -0500

    force https in prod

--- case-management/app/Providers/AppServiceProvider.php
+++ case-management/app/Providers/AppServiceProvider.php
@@ -23,6 +23,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
+
         // Administrator has all permissions
         Gate::before(function ($user, $ability) {
             if ($user->hasRole('Administrator')) {
